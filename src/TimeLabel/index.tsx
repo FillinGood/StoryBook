78c5572a5222670e7b5cc0fdd11f04bab3bb551d
FillinGood <fargust97@gmail.com>
2022-04-25 02:00:08 +0700

    TimeLabel comments

--- src/TimeLabel/index.tsx
+++ src/TimeLabel/index.tsx
@@ -4,11 +4,28 @@ import moment, { LocaleSpecifier } from 'moment';
 import './style.less';
 
 export interface TimeLabelProps {
+  /** Root element className */
   className?: string;
+  /** Display datetime */
   time: Date;
+  /** if true, display in relative format */
   relative?: boolean;
+  /**
+   * if true, display date too.
+   *
+   * Works only with `relative == false`
+   */
   withDate?: boolean;
+  /**
+   * if true, display seconds.
+   *
+   * Works only with `relative == false`
+   */
   withSeconds?: boolean;
+  /** display string locale.
+   *
+   * Defaults to browser language. Fallback = `'en'`
+   */
   locale?: LocaleSpecifier;
   children?: never;
 }
@@ -32,6 +49,7 @@ function fmt(
   return t;
 }
 
+/** Relative/absolute DateTime display. */
 export default function TimeLabel(props: TimeLabelProps) {
   const { className, time, relative, withDate, withSeconds, locale } = props;
   return (
